7b75de39af76d3841dd29c49c367a5b6687b17dc
Jones-Mutua <84673233+Jones-Mutua@users.noreply.github.com>
2021-08-20 09:06:51 +0300

    first commit

---
+++ app/src/main/java/com/meshachandroidproject/MainActivity.java
@@ -0,0 +1,20 @@
+package com.meshachandroidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Project[] myProjects = new Project( title: "The Black Diamond",
+        descriptin: "In the splashy corridors ",
+        R.drawable.
+        )
+    };
+}
---
+++ app/src/main/java/com/meshachandroidproject/Project.java
@@ -0,0 +1,15 @@
+package com.meshachandroidproject;
+
+public class Project {
+
+
+    public Project(String title, String description, int image) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+    }
+
+    String title;
+    String description;
+    int image;
+}
